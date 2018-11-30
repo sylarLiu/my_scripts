01432ccaf403e00c8dcd6c6408636dbaa5812eec
sylar.liu <sylar.liu65@163.com>
2018-11-30 09:34:30 +0800

    Add a script for installing VSCdoe

---
+++ install/install_VSCode.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# Install Ubuntu make
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update
+sudo apt-get install ubuntu-make
+
+
+# Install Visual Studio Code
+umake ide visual-studio-code
